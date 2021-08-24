4f669d2686b98ab203224b80ce3e4fffd8bed7fb
tuisemo <tuisemo@sina.cn>
2021-08-24 23:41:33 +0800

    创建vue2.0版本应用

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    'browser': true,
-    'node': true,
-    'commonjs': true,
-    'jquery': true,
-    'es6': true
+    node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'arrow-parens': 'off',
-    'valid-jsdoc': 'off',
-    'eol-last': 'off',
-    'array-bracket-spacing': 'off',
-    'no-unused-vars': 'off',
-    'no-else-return': 'off',
-    'strict': 'off',
-    'linebreak-style': 'off',
-    'no-irregular-whitespace': [
-      'error',
-      {
-        'skipComments': true
-      }
-    ],
-    'comma-dangle': [
-      'error',
-      {
-        'arrays': 'only-multiline',
-        'objects': 'only-multiline'
-      }
-    ]
-  },
-  'plugins': [
-    'vue'
-  ],
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,105 +1,18 @@
-<!-- 公共容器 -->
 <template>
-  <!-- <el-collapse v-model="activeNames" @change="handleChange">
-          <template v-for="(item,index) in topicList">
-          <el-collapse-item :title="item.title" :name="index" :key="index">
-          </el-collapse-item>
-          </template>
-        </el-collapse> -->
-  <div class="collapse">
-    <template v-for="(item, index) in topicList">
-      <div class="collapse-item" :key="index">
-        <div class="left-part">
-          <img
-            class="user_avatar"
-            :src="item.author.avatar_url"
-            :alt="item.author.avatar_url"
-          />
-          <span class="reply_count">{{
-            item.reply_count + "/" + item.visit_count
-          }}</span>
-          <span class="mark"
-            ><el-tag size="mini">{{ item.tab }}</el-tag></span
-          >
-          <span>{{ item.title }}</span>
-        </div>
-        <div class="right-part">
-          <span class="last_active_time"></span>
-        </div>
-      </div>
-    </template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import api from '@/api/user'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
 export default {
-  data () {
-    return {
-      activeNames: 0,
-      topicList: [],
-    }
-  },
-
-  components: {},
-
-  computed: {},
-
-  methods: {
-    handleChange () {},
-    getTopics () {
-      api.getTopics({ limit: 20 }).then((res) => {
-        console.log(res)
-        this.topicList = res.data.data
-      })
-    },
-  },
-  beforeMount () {
-    this.getTopics()
-  },
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
 }
 </script>
-<style lang='less' scoped>
-.collapse {
-  border-top: 1px solid #ebeef5;
-  border-bottom: 1px solid #ebeef5;
-  .collapse-item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 48px;
-    line-height: 48px;
-    color: #303133;
-    cursor: pointer;
-    border-bottom: 1px solid #ebeef5;
-    font-size: 13px;
-    font-weight: 500;
-    -webkit-transition: border-bottom-color 0.3s;
-    transition: border-bottom-color 0.3s;
-    outline: 0;
-    .left-part {
-      display: flex;
-      flex: 1;
-      align-items: center;
-    }
-    .right-part {
-      width: 80px;
-    }
-  }
-  .user_avatar {
-    width: 30px;
-    height: 30px;
-    border-radius: 3px;
-  }
-  .reply_count {
-    width: 80px;
-    text-align: center;
-    font-size: 12px;
-    color: #999999;
-  }
-  .mark {
-    width: 50px;
-    justify-content: center;
-  }
-}
-</style>
